318b1059e038b78ca8d88416c95f323b37c1fcb1
Ian Gustafson <igustafson101@gmail.com>
2022-10-12 16:51:23 -0500

    Completed part 2

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
@@ -20,6 +22,41 @@ public class PositionType {
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    //Custom methods
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    // Generated equals and hashCode methods using Intellij
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PositionType that = (PositionType) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    // Basically copied version from Employer.java / Location.java
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if(!(o instanceof PositionType)) return false;
+//        PositionType positionType = (PositionType) o;
+//        return getId() == positionType.getId();
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getId());
+//    }
 
     // Getters and Setters:
 
